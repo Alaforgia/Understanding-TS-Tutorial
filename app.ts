368f0de3586d5317c5efa4a9c9d9aaca67667d4f
Tony LaForgia <alforgia428@gmail.com>
2022-05-28 22:27:03 -0500

    log combinedNames

--- app.ts
+++ app.ts
@@ -2,6 +2,8 @@ function combine(input1: number | string, input2: number | string) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
   }
   return result;
 }
@@ -9,4 +11,5 @@ function combine(input1: number | string, input2: number | string) {
 const combinedAges = combine(30, 26);
 console.log(combinedAges);
 
-const combineNames = combine("Max", "Tony");
+const combinedNames = combine("Max", "Tony");
+console.log(combinedNames);
