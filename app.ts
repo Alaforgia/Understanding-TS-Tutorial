c14f3639c2559e3e16c52e0009ed758a3b7480b2
Tony LaForgia <alforgia428@gmail.com>
2022-05-30 16:07:58 -0500

    replaced params with type aliases

--- app.ts
+++ app.ts
@@ -1,7 +1,8 @@
 // We can use Combinable instead of type number, but that wouldn't make to much sense in that case.
-type Combinable = number;
+type Combinable = number | string;
+type ConversionDescriptor = "as-number" | "as-text";
 
-function combine(input1: number | string, input2: number | string, resultConversion: "as-number" | "as-text") {
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     // '+' in front of the params forces them to be numbers
