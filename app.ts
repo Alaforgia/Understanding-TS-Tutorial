4b90991aed8802a15fd91f145481dbe5cd3b4333
Tony LaForgia <alforgia428@gmail.com>
2022-05-30 16:19:06 -0500

    printResult function to test return

--- app.ts
+++ app.ts
@@ -1,3 +1,8 @@
-function add(n1: number, n2: number) {
+// the number type after the params represents the return value type
+function add(n1: number, n2: number): number {
   return n1 + n2;
+}
+
+function printResult(num: number) {
+  console.log("Result: ", +num);
 }
