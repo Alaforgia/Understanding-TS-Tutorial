bd27656ef773b5dd0ba10465bcafd81616e19b17
Tony LaForgia <alforgia428@gmail.com>
2022-05-29 23:23:02 -0500

    gave resultConv param type string

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-function combine(input1: number | string, input2: number | string, resultConversion) {
+function combine(input1: number | string, input2: number | string, resultConversion: string) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
@@ -8,8 +8,11 @@ function combine(input1: number | string, input2: number | string, resultConvers
   return result;
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedNames = combine("Max", "Tony");
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Tony", "as-text");
 console.log(combinedNames);
