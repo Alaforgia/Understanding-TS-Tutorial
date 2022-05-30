a388a7d4c14fc331509af03d8f03975fe4bce268
Tony LaForgia <alforgia428@gmail.com>
2022-05-30 16:45:51 -0500

    calling addAndHandle with anon func that takes in param result & then logging result to test

--- app.ts
+++ app.ts
@@ -8,6 +8,7 @@ function printResult(num: number): void {
   console.log("Result: ", +num);
 }
 
+// cb(callback) takes in a number, but return void
 function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
   const result = n1 + n2;
   cb(result);
@@ -27,3 +28,7 @@ combineValues = add;
 // combineValues = 5;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
