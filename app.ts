440587805c3d01d4555d16336fa05facd3db0e1b
Tony LaForgia <alforgia428@gmail.com>
2022-05-29 23:50:28 -0500

    literal strings replaced type: string as params

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-function combine(input1: number | string, input2: number | string, resultConversion: string) {
+function combine(input1: number | string, input2: number | string, resultConversion: "as-number" | "as-text") {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     // '+' in front of the params forces them to be numbers
