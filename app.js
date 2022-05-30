f76d9d9c867ebeb0df8720a7b3d9565288f81406
Tony LaForgia <alforgia428@gmail.com>
2022-05-30 16:28:25 -0500

    created combineValues func to test any type

--- app.js
+++ app.js
@@ -2,7 +2,11 @@
 function add(n1, n2) {
     return n1 + n2;
 }
+// Void really just means that this function doesn't have a return type
 function printResult(num) {
     console.log("Result: ", +num);
 }
 printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 // the number type after the params represents the return value type
-function add(n1: number, n2: number): number {
+function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
@@ -9,3 +9,9 @@ function printResult(num: number): void {
 }
 
 printResult(add(5, 12));
+
+let combineValues;
+
+combineValues = add;
+
+console.log(combineValues(8, 8));
