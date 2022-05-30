e025574a6a1203da571a059f11f25b32b24dd5f9
Tony LaForgia <alforgia428@gmail.com>
2022-05-29 23:34:27 -0500

    testing if statement

--- app.ts
+++ app.ts
@@ -5,7 +5,11 @@ function combine(input1: number | string, input2: number | string, resultConvers
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
 const combinedAges = combine(30, 26, "as-number");
