88b2bdd6d3cadc397790311cac63ee127d1ff92f
Tony LaForgia <alforgia428@gmail.com>
2022-05-31 19:34:12 -0500

    made comments on type unknown errors

--- app.ts
+++ app.ts
@@ -1,1 +1,7 @@
 let userInput: unknown;
+let userName: string;
+// The code below does not error w/ type unknown
+userInput = 5;
+userInput = "Tony";
+// unknown is not guaranteed to be a string, so userName (type string) = userInput (type unknown) throws an error in TS.
+userName = userInput;
