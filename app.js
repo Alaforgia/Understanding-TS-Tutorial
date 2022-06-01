81d53dda9327ca8ce768039f43a10adae3594087
Tony LaForgia <alforgia428@gmail.com>
2022-05-31 19:43:37 -0500

    notes on type unknown

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-// the number type after the params represents the return value type
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+// The code below does not error w/ type unknown
+userInput = 5;
+userInput = "Tony";
+// unknown is not guaranteed to be a string, so userName (type string) = userInput (type unknown) throws an error in TS.
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-// Void really just means that this function doesn't have a return type
-function printResult(num) {
-    console.log("Result: ", +num);
-}
-// cb(callback) takes in a number, but return void
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-// Type Function tells JS that what the var receives has to be a function.
-// TS will accept the function printResult, but it is not a function that takes two arguments
-// Function types are created with the arrow function from JS.
-// Below, we are telling TS that combineValues has to take in a function that has two params that are numbers,
-// and returns a number.
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
