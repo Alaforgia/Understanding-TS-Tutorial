fd1055f2af0512541134cdfb5c5e85bf58e9646e
Tony LaForgia <alforgia428@gmail.com>
2022-05-31 22:59:01 -0500

    new app.ts file

--- app.ts
+++ app.ts
@@ -1,17 +1,3 @@
-let userInput: unknown;
-let userName: string;
-// The code below does not error w/ type unknown
-userInput = 5;
-userInput = "Tony";
-// unknown is not guaranteed to be a string, so userName (type string) = userInput (type unknown) throws an error in TS.
-if (typeof userInput === "string") {
-  userName = userInput;
-}
+const userName = "Max";
 
-// Never is "clearer" than void, it is intended to never return something, where as void means that it doesn't have a return type.
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-}
-
-const result = generateError("An error occurred!", 500);
-console.log(result);
+console.log(userName);
--- unknown-never.ts
+++ unknown-never.ts
@@ -1,15 +1,17 @@
-var userInput;
-var userName;
+let userInput: unknown;
+let userName: string;
 // The code below does not error w/ type unknown
 userInput = 5;
 userInput = "Tony";
 // unknown is not guaranteed to be a string, so userName (type string) = userInput (type unknown) throws an error in TS.
 if (typeof userInput === "string") {
-    userName = userInput;
+  userName = userInput;
 }
+
 // Never is "clearer" than void, it is intended to never return something, where as void means that it doesn't have a return type.
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
-var result = generateError("An error occurred!", 500);
+
+const result = generateError("An error occurred!", 500);
 console.log(result);
