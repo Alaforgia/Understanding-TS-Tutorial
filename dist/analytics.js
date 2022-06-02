bd29e9ba4ab454bfda3729eae10e8f5a91fe2609
Tony LaForgia <alforgia428@gmail.com>
2022-06-02 04:42:50 -0500

    testing extra type checking

--- dist/analytics.js
+++ dist/analytics.js
@@ -1,5 +1,8 @@
 "use strict";
+// type Any by default
+let logged;
 function sendAnalytics(data) {
     console.log(data);
+    logged = true;
 }
 sendAnalytics("The data");
--- src/analytics.ts
+++ src/analytics.ts
@@ -1,5 +1,9 @@
+// type Any by default
+let logged;
+
 function sendAnalytics(data: string) {
   console.log(data);
+  logged = true;
 }
 
 sendAnalytics("The data");
